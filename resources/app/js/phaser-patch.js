f1aafdae7459742126f2c5be1ced4d0bdd9899ea
DamienBlack <damien.r.black@gmail.com>
2018-04-06 14:38:27 -0600

    Fullscreen and shield utilities

--- resources/app/js/phaser-patch.js
+++ resources/app/js/phaser-patch.js
@@ -14,3 +14,45 @@ Phaser.StateManager.prototype.setCurrentState = function(key) {
         }
     }
 };
+
+Phaser.Sprite.prototype.fullScreen = function() {
+    this.fixedToCamera = false;
+
+    this.anchor.setTo(.5);
+    this.position.x = game.camera.width / 2;
+    this.position.y = game.camera.height / 2;
+
+    let spriteRatio = this.width/this.height;
+    let screenRatio = world.electronPhaserSettings.canvasResolution[0] / world.electronPhaserSettings.canvasResolution[1];
+
+    let scaleFactor;
+    if (spriteRatio >= screenRatio) {
+        scaleFactor = world.electronPhaserSettings.canvasResolution[0] / this.width;
+    } else {
+        scaleFactor = world.electronPhaserSettings.canvasResolution[1] / this.height;
+    }
+
+
+    this.setScale(scaleFactor);
+};
+
+Phaser.Group.prototype.fullScreen = function() {
+    this.fixedToCamera = false;
+
+    let spriteRatio = this.width/this.height;
+    let screenRatio = world.electronPhaserSettings.canvasResolution[0] / world.electronPhaserSettings.canvasResolution[1];
+
+    let scaleFactor;
+    if (spriteRatio >= screenRatio) {
+        scaleFactor = world.electronPhaserSettings.canvasResolution[0] / this.width;
+    } else {
+        scaleFactor = world.electronPhaserSettings.canvasResolution[1] / this.height;
+    }
+
+    this.scale.set(scaleFactor, scaleFactor);
+
+    this.position.x = game.camera.width / 2 - this.width / 2;
+    this.position.y = game.camera.height / 2 - this.height / 2;
+
+    console.log(this.children);
+};
--- resources/app/js/utilities.js
+++ resources/app/js/utilities.js
@@ -3,3 +3,18 @@ function closeWindow() {
     let window = remote.getCurrentWindow();
     window.close();
 }
+
+function raiseWindowShield(color) {
+    if (world.windowShield) {
+        world.windowShield.destroy();
+    }
+    let graphics = game.add.graphics(0, 0);
+    graphics.beginFill(color);
+    world.windowShield = graphics.drawRect(0, 0, game.world.width, game.world.height);
+}
+
+function lowerWindowShield() {
+    if (world.windowShield) {
+        world.windowShield.destroy();
+    }
+}
