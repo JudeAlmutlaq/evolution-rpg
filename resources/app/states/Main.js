2aed68ee32774838003631f45958d3b16e8886d1
Judealmutlaq <jude.almulaq@gmail.com>
2018-04-06 22:08:33 -0600

    Basic Pause Menu up

--- resources/app/states/Main.js
+++ resources/app/states/Main.js
@@ -20,19 +20,19 @@ class State {
 
         game.stage.backgroundColor = "#0d2b00";
 
-        jungle = game.add.sprite(game.world.centerX, game.world.centerY, 'jungle_1')
+        jungle = game.add.sprite(game.camera.width/2, game.camera.height/2, 'jungle_1');
 
         jungle.anchor.setTo(0.5);
 
         jungle.scale.x = 1.5;
         jungle.scale.y = 1.5;
 
-        button = game.add.image(game.world.centerX, game.world.centerY, 'button' );
+        button = game.add.image(game.camera.width/2, game.camera.height/2, 'button' );
         button.anchor.setTo(0.5);
         button.inputEnabled = true;
         button.events.onInputDown.add(toTown);
 
-        exitButton = game.add.image(game.world.centerX, game.world.centerY+150, 'exitButton' );
+        exitButton = game.add.image(game.camera.width/2, game.camera.height/2+150, 'exitButton' );
         exitButton.anchor.setTo(0.5);
         exitButton.inputEnabled = true;
         exitButton.events.onInputDown.add(closeWindow);
