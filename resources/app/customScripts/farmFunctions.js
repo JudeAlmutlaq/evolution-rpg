0f789e4b5645d3b21daf4e695b433bdb277a1554
Judealmutlaq <jude.almulaq@gmail.com>
2018-10-20 18:05:54 -0600

    talk prompt switched to object

--- resources/app/customScripts/farmFunctions.js
+++ resources/app/customScripts/farmFunctions.js
@@ -1,28 +1,31 @@
 class FarmFunctions extends MovementFunctions {
 
     clickItem(sprite, pointer, itemInfo, itemNumber) {
+        var jsondata = fs.readFileSync('./resources/app/images/farm.json');
+        this.mapInfo = JSON.parse(jsondata);
+
         if (itemInfo.itemType === 'pen' && this.talkZone){
             sprite.destroy();
             world.inventory.splice(itemNumber, 1);
             this.openInventory();
             this.openInventory();
             this.penInHand = true;
-            this.createPen();
+            this.createPen(this.mapInfo.layers[2].objects.properties);
         }
     }
 
-    talkPrompt() {
-        console.log(world.playerX, world.playerY);
-        let textStyle = { font: "50px Arial", fill: "#fff", align: "center" };
-        this.talkPromptText = game.add.text(165, 155, "!", textStyle, this.farmGraphics);
-        this.talkZone = true;
-    }
+    // talkPrompt() {
+    //     console.log(world.playerX, world.playerY);
+    //     let textStyle = { font: "50px Arial", fill: "#fff", align: "center" };
+    //     this.talkPromptText = game.add.text(165, 155, "!", textStyle, this.farmGraphics);
+    //     this.talkZone = true;
+    // }
 
-    removeTalkPrompt(){
-        this.talkPromptText.destroy();
-        this.talkZone = false;
-        this.talkPromptText = null;
-    }
+    // removeTalkPrompt(){
+    //     this.talkPromptText.destroy();
+    //     this.talkZone = false;
+    //     this.talkPromptText = null;
+    // }
 
     createPen () {
         if (this.penInHand && this.talkZone){
@@ -32,10 +35,15 @@ class FarmFunctions extends MovementFunctions {
     }
 
     beginObjectCollision(properties) {
-        console.log("I've collided", properties);
+        console.log(properties);
+        let textStyle = { font: "50px Arial", fill: "#fff", align: "center" };
+        this.talkPromptText = game.add.text(properties.xPos*32+40, properties.yPos*32+80, "!", textStyle, this.farmGraphics);
+        this.talkZone = true;
     };
 
     endObjectCollision(properties) {
-        console.log("not");
+        this.talkPromptText.destroy();
+        this.talkZone = false;
+        this.talkPromptText = null;
     };
 }
